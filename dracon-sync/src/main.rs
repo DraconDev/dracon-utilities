87caf7a39cf079ffd50b37b44d7fbd8dccf138ee
DraconDev <dracsharp@gmail.com>
2026-02-27 06:27:37 +0000

    sync: 1 modified (dracon-sync/src/main.rs in dracon-utilities)

--- dracon-sync/src/main.rs
+++ dracon-sync/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
 use dracon_git::{
     build_commit_message, CommitContext,
-    extract_intent, scan_plan_tasks, GitService,
+    extract_intent, scan_blueprint_tasks, GitService,
 };
 use dracon_git::types::{DiffFile, RepoStatus};
 use fs2::FileExt;
