870440c9123c553ab42dd80621aef0df31da7969
test <test@test.com>
2026-05-29 11:19:05 +0100

    chore(sync): update dracon-sync.example, report

--- dracon-sync/src/report.rs
+++ dracon-sync/src/report.rs
@@ -2422,7 +2422,6 @@ mod tests {
             nix_auto_update: false,
             standard_files: vec![],
             standard_files_auto: true,
-            todo_commit_messages: false,
         }
     }
 
